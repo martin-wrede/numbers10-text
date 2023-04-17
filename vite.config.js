eb6057269e233f95103ea9ae9bc3661a02e46cc9
martin-wrede <81897938+martin-wrede@users.noreply.github.com>
2023-04-17 15:18:29 +0200

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base:'/numbers10-text/',
-  plugins: [react()]
-})
